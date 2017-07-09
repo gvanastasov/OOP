e28486f7b48878df5d9235e5787810dda58b718e
gva <gvanastasov@abv.bg>
2017-07-09 18:40:40 +0200

    defined person class

---
+++ OOP/Def_Person/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Def_Person
+{
+    public class Person
+    {
+
+    }
+}
---
+++ OOP/Define_Bank_Acc/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Define_Bank_Acc
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        private List<BankAccount> accounts;
+
+        public Person(string name, int age)
+            : this(name, age, new List<BankAccount>())
+        {
+        }
+
+        public Person(string name, int age, List<BankAccount> accounts) 
+        {
+            this.name = name;
+            this.age = age;
+            this.accounts = accounts;
+        }
+
+        public double GetBalance()
+        {
+            return accounts.Sum(x => x.Balance);
+        }
+    }
+}
