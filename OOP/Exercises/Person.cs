fdced6ebd15b370a23309bc6fd2776adba925b10
gva <gvanastasov@abv.bg>
2017-07-09 18:51:50 +0200

    ex 1 and 2 done

---
+++ OOP/Exercises/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+
+        public Person() : this(1, "No name")
+        {
+        }
+
+        public Person(int age) : this(age, "No name")
+        {
+        }
+
+        public Person(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+
+    }
+}
---
+++ OOP/Exercises/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            // ex.1
+            Type personType = typeof(Person);
+            FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+
+            var pesho = new Person();
+            pesho.age = 20;
+            var gosho = new Person() { age = 18 };
+            var stamat = new Person() { age = 43, name = "Stamat" };
+
+            // ex.2
+            ConstructorInfo emptyCtor = personType.GetConstructor(new Type[] { });
+            ConstructorInfo ageCtor = personType.GetConstructor(new[] { typeof(int) });
+            ConstructorInfo nameAgeCtor = personType.GetConstructor(new[] { typeof(string), typeof(int) });
+            bool swapped = false;
+            if (nameAgeCtor == null)
+            {
+                nameAgeCtor = personType.GetConstructor(new[] { typeof(int), typeof(string) });
+                swapped = true;
+            }
+
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person basePerson = (Person)emptyCtor.Invoke(new object[] { });
+            Person personWithAge = (Person)ageCtor.Invoke(new object[] { age });
+            Person personWithAgeAndName = swapped ? (Person)nameAgeCtor.Invoke(new object[] { age, name }) : (Person)nameAgeCtor.Invoke(new object[] { name, age });
+
+            Console.WriteLine("{0} {1}", basePerson.name, basePerson.age);
+            Console.WriteLine("{0} {1}", personWithAge.name, personWithAge.age);
+            Console.WriteLine("{0} {1}", personWithAgeAndName.name, personWithAgeAndName.age);
+
+            // ex.3
+        }
+    }
+}
