6035e1dc3785c6e1f5adccc23bdf4fda7b7f7522
gva <gvanastasov@abv.bg>
2017-07-09 22:11:04 +0200

    base ex8

---
+++ OOP/RawData/Cargo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawData
+{
+    public class Cargo
+    {
+        public double Weight { get; set; }
+        public CargoType CargoType { get; set; }
+
+        public Cargo(double weight, CargoType type)
+        {
+            this.Weight = weight;
+            this.CargoType = type;
+        }
+    }
+}
