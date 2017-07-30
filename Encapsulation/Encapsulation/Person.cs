11719452238d7d679f2fd9635e373c72a097a690
gva <gvanastasov@abv.bg>
2017-07-30 12:17:51 +0200

    task 4 classes

---
+++ Encapsulation/Encapsulation/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Person
+    {
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.products = new List<Product>();
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Name cannot be empty.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        private decimal money;
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new Exception("Money cannot be negative.");
+                }
+
+                this.money = value;
+            }
+        }
+
+        private List<Product> products;
+        public IReadOnlyList<Product> Products
+        {
+            get
+            {
+                return this.Products;
+            }
+        }
+
+        public void BuyProduct(Product product)
+        {
+            this.Money -= product.Cost;
+            this.products.Add(product);
+        }
+
+        public string GetCart()
+        {
+            return products.Count == 0
+                ? "Nothing bought" 
+                : string.Join(", ", this.products.Select(p => p.Name));
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Product
+    {
+        public Product(string name, decimal cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Name cannot be empty.");
+                }
+                this.name = value;
+            }
+        }
+
+        private decimal cost;
+        public decimal Cost
+        {
+            get
+            {
+                return this.cost;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new Exception("Cost cannot be negative.");
+                }
+
+                this.cost = value;
+            }
+        }
+    }
+}
