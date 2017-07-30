81db6e8f6bd8d2e184740413eb67e57bad0e37f5
gva <gvanastasov@abv.bg>
2017-07-30 16:44:07 +0200

    task 1 done

---
+++ Inheritance/Inheritance/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Inheritance.Person;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Startup
+    {
+        static void Main()
+        {
+            Task1();
+        }
+
+        private static void Task1()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
