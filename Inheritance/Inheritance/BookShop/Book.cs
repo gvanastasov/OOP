9a06000bc23baa7fc57f8b86ec4700a4affce388
gva <gvanastasov@abv.bg>
2017-07-30 20:37:23 +0200

    models

---
+++ Inheritance/Inheritance/BookShop/Book.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Inheritance.BookShop
+{
+    public class Book
+    {
+        public Book(string author, string title, decimal price)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Price = price;
+        }
+
+        protected string title;
+        public string Title
+        {
+            get
+            {
+                return this.title;
+            }
+            set
+            {
+                if(value.Length < 3)
+                {
+                    throw new Exception("Title not valid!");
+                }
+
+                this.title = value;
+            }
+        }
+
+        protected string author;
+        public string Author
+        {
+            get
+            {
+                return this.author;
+            }
+            set
+            {
+                var tokens = value.Split(' ');
+                var secondName = tokens[1];
+
+                if (Regex.IsMatch(secondName[0].ToString(), @"^\d$"))
+                {
+                    throw new Exception("Author not valid!");
+                }
+
+                this.author = value;
+            }
+        }
+
+        protected decimal price;
+        public virtual decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new Exception("Price not valid!");
+                }
+
+                this.price = Math.Round(value, 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("Type: ").AppendLine(this.GetType().Name)
+                .Append("Title: ").AppendLine(this.Title)
+                .Append("Author: ").AppendLine(this.Author)
+                .Append("Price: ").Append($"{this.Price:f1}")
+                .AppendLine();
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Inheritance/Inheritance/BookShop/GoldenEditionBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.BookShop
+{
+    public class GoldenEditionBook : Book
+    {
+        public GoldenEditionBook(string author, 
+            string title, decimal price) : base(author, title, price)
+        {
+        }
+
+        public override decimal Price
+        {
+            get
+            {
+                return base.Price * 1.3m;
+            }
+
+            set
+            {
+                base.Price = value;
+            }
+        }
+    }
+}
