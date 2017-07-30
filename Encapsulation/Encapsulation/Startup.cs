da3fa962466313356ee1f22ec4e38b7b5b414517
gva <gvanastasov@abv.bg>
2017-07-30 12:17:56 +0200

    task 4 logic

--- Encapsulation/Encapsulation/Startup.cs
+++ Encapsulation/Encapsulation/Startup.cs
@@ -11,7 +11,7 @@ namespace Encapsulation
     {
         static void Main()
         {
-            Task1();
+            Task3();
         }
 
         private static void Task1()
@@ -26,6 +26,86 @@ namespace Encapsulation
 
             var box = new Box(l, w, h);
             Console.WriteLine(box.ToString());
+        }
+
+        private static void Task2()
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            var chicken = new Chicken(name, age);
+
+            Console.WriteLine(chicken.ToString());
+        }
+
+        private static void Task3()
+        {
+            var people = new List<Person>();
+            var products = new List<Product>();
+
+            try
+            {
+                var peopleString = Console.ReadLine();
+                var peopleTokens = peopleString.Split(new string[] { ";", "=" }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < peopleTokens.Length; j+=2)
+                {
+                    var name = peopleTokens[j];
+                    var money = decimal.Parse(peopleTokens[j+1]);
+
+                    var person = new Person(name, money);
+                    people.Add(person);
+                }
+
+                var productString = Console.ReadLine();
+                var productTokens = productString.Split(new string[] { ";", "=" }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < productTokens.Length; i+=2)
+                {
+                    var name = productTokens[i];
+                    var cost = decimal.Parse(productTokens[i + 1]);
+
+                    var product = new Product(name, cost);
+                    products.Add(product);
+                }
+
+                var output = new StringBuilder();
+                while (true)
+                {
+                    var cmd = Console.ReadLine();
+
+                    if(cmd.ToLower() == "end")
+                    {
+                        break;
+                    }
+
+                    var orderTokens = cmd.Split(' ');
+
+                    var person = people.FirstOrDefault(p => p.Name == orderTokens[0]);
+                    var product = products.FirstOrDefault(p => p.Name == orderTokens[1]);
+
+                    if(person.Money >= product.Cost)
+                    {
+                        person.BuyProduct(product);
+                        output.AppendLine($"{person.Name} bought {product.Name}");
+                    }
+                    else
+                    {
+                        output.AppendLine($"{person.Name} can't afford {product.Name}");
+                    }
+                }
+
+                foreach (var person in people)
+                {
+                    output.AppendLine($"{person.Name} - {person.GetCart()}");
+                }
+
+                Console.WriteLine(output.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
