646c240ee9e3422a6ab67b2a4e87839c7eaa0a67
gva <gvanastasov@abv.bg>
2017-07-30 11:14:18 +0200

    task 1 done

---
+++ Encapsulation/Encapsulation/Box.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Box
+    {
+        public Box(double len, double w, double h)
+        {
+            this.length = len;
+            this.width = w;
+            this.height = h;
+        }
+
+        private double length;
+        public double Length
+        {
+            get { return this.length; }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new Exception("Length cannot be negative.");
+                }
+                this.length = value;
+            }
+        }
+
+        private double width;
+        public double Width
+        {
+            get { return this.width; }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new Exception("Width cannot be negative.");
+                }
+                this.width = value;
+            }
+        }
+
+        private double height;
+        public double Height
+        {
+            get { return this.height; }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new Exception("Height cannot be negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        public double GetSurfaceArea()
+        {
+            return 2 * (this.height * this.width) +
+                   2 * (this.height * this.length) +
+                   2 * (this.width * this.length);
+        }
+
+        public double GetLateralSurfaceArea()
+        {
+            return 2 * (this.height * this.width) +
+                   2 * (this.height * this.length);
+        }
+
+        public double GetVolume()
+        {
+            return this.height * this.width * this.length;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Surface Area - {GetSurfaceArea():f2}");
+            sb.AppendLine($"Lateral Area - {GetLateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {GetVolume():f2}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Startup
+    {
+        static void Main()
+        {
+            Task1();
+        }
+
+        private static void Task1()
+        {
+            var l = double.Parse(Console.ReadLine());
+            var w = double.Parse(Console.ReadLine());
+            var h = double.Parse(Console.ReadLine());
+
+            Type boxType = typeof(Box);
+            FieldInfo[] fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine(fields.Count());
+
+            var box = new Box(l, w, h);
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
