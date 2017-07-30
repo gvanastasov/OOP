01dfebeb69ad8aa9149b482814617476481d57af
gva <gvanastasov@abv.bg>
2017-07-30 14:23:25 +0200

    enum types

---
+++ Encapsulation/Encapsulation/PizzaCalories/Enums/BakingTechniqueType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.PizzaCalories.Enums
+{
+    public enum BakingTechniqueType
+    {
+        Crispy,
+        Chewy,
+        Homemade
+    }
+}
